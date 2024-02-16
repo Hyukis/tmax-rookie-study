29f7cd70c1d52ba51299b09f07e697245323e441
endelssmomo <soohyuk96@naver.com>
2024-02-16 17:11:24 +0900

    [build] SpringBoot + Mybatis 학습을 위한 환경 셋팅

---
+++ src/main/java/com/example/tmaxrookiestudy/TmaxRookieStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmaxrookiestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmaxRookieStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TmaxRookieStudyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/tmaxrookiestudy/TmaxRookieStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmaxrookiestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmaxRookieStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
