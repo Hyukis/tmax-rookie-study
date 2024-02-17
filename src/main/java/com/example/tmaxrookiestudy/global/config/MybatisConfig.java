2e5677cdf244319a87293a955251ed1be7490c7b
endelssmomo <soohyuk96@naver.com>
2024-02-17 12:37:35 +0900

    [feat] Mybatis-Config 등록 xml을 통한 매퍼를 사용하려면 주석을 해제해야한다.

---
+++ src/main/java/com/example/tmaxrookiestudy/global/config/MybatisConfig.java
@@ -0,0 +1,46 @@
+package com.example.tmaxrookiestudy.global.config;
+
+import com.example.tmaxrookiestudy.employee.domain.typeHandler.DepartmentHandler;
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.TypeHandler;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(value = "com.example.tmaxrookiestudy", sqlSessionFactoryRef = "sqlSessionFactory")
+public class MybatisConfig {
+
+  @Value("${spring.datasource.mybatis.mapper-locations}")
+  String mapperPath;
+
+  @Bean(name = "dataSource")
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean(name = "sqlSessionFactory")
+  public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource
+      , ApplicationContext applicationContext) throws Exception {
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    sqlSessionFactoryBean.setDataSource(dataSource);
+    // xml 파일에 쿼리문을 매핑할 때 사용한다.
+    // sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperPath));
+    return sqlSessionFactoryBean.getObject();
+  }
+
+  @Bean(name = "sqlSessionTemplate")
+  public SqlSessionTemplate sqlSessionTemplate(
+      @Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    return new SqlSessionTemplate(sqlSessionFactory);
+  }
+}
